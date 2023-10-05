e83ccf6eafc5ac9362fc3161657a1c3921e5c2ef
Hady Awayda <hady_awayda@hotmail.com>
2023-10-05 21:26:41 +0300

    Fixed UI

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 $(function () {
-  let position = { x: undefined, y: undefined };
   let count = 0;
-
-  window.addEventListener("mousemove", (e) => {
-    position.x = e.clientX;
-    position.y = e.clientY;
-  });
-  // $(".left").on( "mouseenter", function() {
-  //   $(".left").toggle( "highlight" );
-  // });
-  rect = document.querySelector(".default").getBoundingClientRect();
 
   $(".draggable").draggable({
     revert: true,
   });
 
-  $(".draggable").on("mouseup", function () {
-    if (rect.y < position.y && rect.y + 150 > position.y) {
-      if (rect.x < position.x && rect.x + 75 > position.x) {
-        $(".output").prepend(`<div class="box new">BOX #${++count}</div>`);
-        rect = document.querySelector(".default").getBoundingClientRect();
-      }
-      if (rect.x + 75 < position.x && rect.x + 150 > position.x) {
-        $(".output").append(`<div class="box new">BOX #${++count}</div>`);
-        rect = document.querySelector(".default").getBoundingClientRect();
-      }
-    }
+  $(".right").droppable({
+    drop: function () {
+      $(".output").append(`<div class="box new">BOX #${++count}</div>`);
+    },
+  });
+
+  $(".left").droppable({
+    drop: function () {
+      $(".output").prepend(`<div class="box new">BOX #${++count}</div>`);
+    },
   });
 });
